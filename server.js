17822aa2bbe4b90552fcdd17ca7c41af9f2bccbc
VaishuGK242 <vaishugk542@gmail.com>
2025-03-19 15:48:22 +0530

    third

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ const router = require('./routes/authRoutes');
 const cartrouter = require('./routes/cartRoutes');
 
 const app = express();
+const allowedOrigins = ["ecom-frontend-36xz5h6ow-vaishnavi-g-ks-projects.vercel.app" , "ecom-frontend-ochre.vercel.app"]
 
-app.use(cors())
+app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Allows cookies and authentication headers if needed
+  }))
 app.use(express.json())
 
 
